02a55c0a57a06f2de105937632355eb09beedb3d
hhhjin <kkp7125@gmail.com>
2024-11-07 17:03:22 +0900

    fix: module not found

--- src/app/[ai]/page.tsx
+++ src/app/[ai]/page.tsx
@@ -1,4 +1,3 @@
-import { DallE } from "@/components/dall-e/dall-e";
 import { Ideogram } from "@/components/ideogram/ideogram";
 import { OpenAi } from "@/components/openai/openai";
 import { Recraft } from "@/components/recraft/recraft";
@@ -7,9 +6,6 @@ import { redirect } from "next/navigation";
 export default function AiPage({ params }: { params: { ai: string } }) {
   if (params.ai === "openai") {
     return <OpenAi />;
-  }
-  if (params.ai === "dall-e") {
-    return <DallE />;
   }
   if (params.ai === "recraft") {
     return <Recraft />;
