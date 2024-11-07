20ba335e2f11f4a1f2f841d44a637443fc4a3015
hhhjin <kkp7125@gmail.com>
2024-11-07 22:57:57 +0900

    enhance: smaller logo

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -4,7 +4,7 @@ import { FeedbackPopover } from "./feedback-popover";
 export function Header() {
   return (
     <div className="flex justify-between items-center p-4">
-      <h1 className="text-2xl font-semibold">
+      <h1 className="text-xl font-semibold">
         <Link href="/" className="hover:opacity-80">
           Gennmix
         </Link>
