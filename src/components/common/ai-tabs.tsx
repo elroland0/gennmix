716ea197a0bd61b3e4e7ba8dd10aa547794ec676
hhhjin <kkp7125@gmail.com>
2024-11-09 22:41:12 +0900

    ux: contrast to ai tabs

--- src/components/common/ai-tabs.tsx
+++ src/components/common/ai-tabs.tsx
@@ -6,17 +6,29 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export function AiTabs({ ai }: { ai: string }) {
   return (
     <Tabs value={ai}>
-      <TabsList className="text-neutral-700">
-        <TabsTrigger value="openai">
+      <TabsList className="text-neutral-700 font-normal gap-1">
+        <TabsTrigger
+          value="openai"
+          className="hover:bg-neutral-900 hover:text-neutral-50 data-[state=active]:bg-neutral-900 data-[state=active]:text-neutral-50"
+        >
           <Link href="/openai">OpenAI</Link>
         </TabsTrigger>
-        <TabsTrigger value="recraft">
+        <TabsTrigger
+          value="recraft"
+          className="hover:bg-neutral-900 hover:text-neutral-50 data-[state=active]:bg-neutral-900 data-[state=active]:text-neutral-50"
+        >
           <Link href="/recraft">Recraft</Link>
         </TabsTrigger>
-        <TabsTrigger value="ideogram">
+        <TabsTrigger
+          value="ideogram"
+          className="hover:bg-neutral-900 hover:text-neutral-50 data-[state=active]:bg-neutral-900 data-[state=active]:text-neutral-50"
+        >
           <Link href="/ideogram">Ideogram</Link>
         </TabsTrigger>
-        <TabsTrigger value="black-forest-labs">
+        <TabsTrigger
+          value="black-forest-labs"
+          className="hover:bg-neutral-900 hover:text-neutral-50 data-[state=active]:bg-neutral-900 data-[state=active]:text-neutral-50"
+        >
           <Link href="/black-forest-labs">Black Forest Labs</Link>
         </TabsTrigger>
       </TabsList>
